f73482942450d89578c6b5db7abc2b0ecd2022e8
Nic O'Connor <noconnor@cert.org>
2022-01-20 09:33:59 -0500

    adds prep script to install dependancies - Updates setup to remove redundant code - adds task to ansible for copying kubectl config - updates utils with prep.sh functions

--- common/certs/generate-certs.sh
+++ common/certs/generate-certs.sh
@@ -3,7 +3,19 @@
 # Copyright 2021 Carnegie Mellon University.
 # Released under a BSD (SEI)-style license, please see LICENSE.md in the
 # project root or contact permission@sei.cmu.edu for full terms.
+[ `whoami` = root ] || { sudo "$0" "$@"; exit $?; }
+# Install cfssl 
+CFSSL=/usr/local/bin/cfssl
+CFSSLJSON=/usr/local/bin/cfssljson
 
+if [ ! -f "$CFSSL" ]; then
+  sudo curl -L https://pkg.cfssl.org/R1.2/cfssl_linux-amd64 --output $CFSSL
+fi
+
+if [ ! -f "$CFSSLJSON" ]; then
+  sudo curl -L https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64 --output $CFSSLJSON
+fi
+sudo chmod +x $CFSSL $CFSSLJSON
 
 # Change to the current directory
 cd "$(dirname "${BASH_SOURCE[0]}")"
